aeac21dff039ae3465b827840389cc4d075cabdf
goes.rafaelasilva@gmail.com <goes.rafaelasilva@gmail.com>
2025-02-22 13:15:55 -0300

    feat: add shadon initial setup and componets

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductsPage = () => {
+  return (
+    <div className="p-5 border-red-500 rounded-x1">
+      <h1 className="text-red-500">Product page</h1>
+      <Button>FSW 7.0</Button>
+      <Input />
+    </div>
+  );
+};
+
+export default ProductsPage;
